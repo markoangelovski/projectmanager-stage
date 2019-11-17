0aee90429f1b584523f87ad2d43b17dd0d1044e0
Angelovski <marko.angelovsk@gmail.com>
2019-11-17 07:01:42 +0100

    Updates

---
+++ assets/app-init-chunk-662795cfcb479e705951.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{20:function(t,e){const s=document.createElement("script");s.setAttribute("type","text/javascript"),s.setAttribute("src","assets/js/kanboard.js"),document.getElementsByTagName("body")[0].appendChild(s)}}]);
