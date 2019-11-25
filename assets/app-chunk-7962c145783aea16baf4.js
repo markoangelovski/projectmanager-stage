d98072738d48cfcd386193cfac3612c1de69c8cc
Angelovski <marko.angelovsk@gmail.com>
2019-11-25 23:31:00 +0100

    Updates

--- assets/app-chunk-7962c145783aea16baf4.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{25:function(n,e,l){l.e(3).then(l.t.bind(null,20,7)),Promise.all([l.e(1),l.e(0),l.e(8)]).then(l.bind(null,21)),l.e(4).then(l.t.bind(null,23,7)),Promise.all([l.e(1),l.e(0),l.e(7)]).then(l.bind(null,24))}}]);
